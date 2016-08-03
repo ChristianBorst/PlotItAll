fc3f656df83a535320b6345f951a0b5867ec65c9
Christian Borst <cborst@wisc.edu>
2016-08-03 13:21:00 -0500

    Planning the general algorithm

---
+++ plotitall.py
@@ -0,0 +1,15 @@
+"""Desired goal: A script which takes in any formatted data filename 
+and a format specification, then produces a generic plot of that 
+data with colors and symbols."""
+
+#TODO: Verify these comments form the algorithm you'd like to use.
+#TODO: Mentally test this algo
+
+#Read format specifier
+
+#Using the format specifier, line-by-line add the data to column variables
+
+#Use the format specifier to decipher the file contents and split into columns
+
+#Have matplotlib plot the data, with the x and y functions given in the format specifier
+#Write the plot to the output file specified
